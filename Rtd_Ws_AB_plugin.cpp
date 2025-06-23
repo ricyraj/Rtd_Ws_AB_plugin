a310ec49e5f02af02c59088cc0d3e3949ca833b2
ricyraj <ricyraj@gmail.com>
2025-06-23 21:45:04 +0530

    Add files via upload

---
+++ Rtd_Ws_AB_plugin.cpp
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <stdio.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        MessageBoxA(0, "DLL Loaded Successfully!", "RTD Plugin", MB_OK);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
